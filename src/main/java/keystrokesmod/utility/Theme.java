43e14bac148f029a6d66fa9e5575d949158dbeb9
Keg097 <davidchil2007@gmail.com>
2025-02-17 22:18:50 -0500

    Added all of the latest commits from normal raven bs

--- src/main/java/keystrokesmod/utility/Theme.java
+++ src/main/java/keystrokesmod/utility/Theme.java
@@ -14,7 +14,8 @@ public enum Theme {
     Grayscale(new Color(240, 240, 240), new Color(110, 110, 110)), // 6
     Royal(new Color(125, 204, 241), new Color(30, 71, 170)), // 7
     Sky(new Color(160, 230, 225), new Color(15, 190, 220)), // 8
-    Vape(new Color(21, 137, 122), new Color(44, 72, 79)); // 9
+    Vine(new Color(17, 192, 45), new Color(201, 234, 198)); // 9
+    Vape(new Color(21, 137, 122), new Color(44, 72, 79)); // 10
 
     private final Color firstGradient;
     private final Color secondGradient;
@@ -31,18 +32,25 @@ public enum Theme {
             return convert(values()[index].firstGradient, values()[index].secondGradient, (Math.sin(System.currentTimeMillis() / 1.0E8 * Settings.timeMultiplier.getInput() * 400000.0 + delay * Settings.offset.getInput()) + 1.0) * 0.5).getRGB();
         }
         else if (index == 0) {
-            return Utils.getChroma(2, (long) delay);
+            return getChromaOffset(2, (long) delay);
         }
         return -1;
+    }
+
+    public static int getChromaOffset(long speed, long delay) {
+        double offset = Settings.offset.getInput() * delay;
+        long time = System.currentTimeMillis() + (long) offset;
+        float hue = (float) (time % (15000L / speed)) / (15000.0F / (float) speed);
+        return Color.getHSBColor(hue, 1.0F, 1.0F).getRGB();
     }
 
     public static int getGradient(Color firstGradient, Color secondGradient, double delay) {
         return convert(firstGradient, secondGradient, (Math.sin(System.currentTimeMillis() / 1.0E8 * 0.5 * 400000.0 + delay * 0.550000011920929) + 1.0) * 0.5).getRGB();
     }
 
-    public static Color convert(Color color, Color color2, double n) {
-        double n2 = 1.0 - n;
-        return new Color((int) (color.getRed() * n + color2.getRed() * n2), (int) (color.getGreen() * n + color2.getGreen() * n2), (int) (color.getBlue() * n + color2.getBlue() * n2));
+    public static Color convert(Color color, Color color2, double delay) {
+        double n2 = 1.0 - delay;
+        return new Color((int) (color.getRed() * delay + color2.getRed() * n2), (int) (color.getGreen() * delay + color2.getGreen() * n2), (int) (color.getBlue() * delay + color2.getBlue() * n2));
     }
 
     public static int[] getGradients(int index) {
@@ -59,5 +67,5 @@ public enum Theme {
         return new int[]{0, 0};
     }
 
-    public static String[] themes = new String[]{"Rainbow", "Cherry", "Cotton candy", "Flare", "Flower", "Gold", "Grayscale", "Royal", "Sky", "Vape"};
+    public static String[] themes = new String[]{"Rainbow", "Cherry", "Cotton candy", "Flare", "Flower", "Gold", "Grayscale", "Royal", "Sky", "Vine", "Vape"};
 }
